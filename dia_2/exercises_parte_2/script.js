19f0c1ecf68a4f7821fd6b41a4de78fc97a2cacf
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-25 10:46:31 -0300

    Exercicio 4 da parte II - Feito

--- dia_2/exercises_parte_2/script.js
+++ dia_2/exercises_parte_2/script.js
@@ -35,5 +35,8 @@ const lesson1 = {
   // Exercício 3
   const comprimentoDoObjeto = (objeto) => Object.keys(objeto).length;
   console.log(comprimentoDoObjeto(lesson1));
+
   // Exercício 4
+  const listandoValores = (objeto) => Object.values(objeto);
+  console.log(listandoValores(lesson2));
   // Exercício 5
