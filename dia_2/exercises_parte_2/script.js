d5120081ebe81228ab43a80ccd8c69b21faf8513
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-25 11:30:07 -0300

    Exercicio 6 da parte II - Feito

--- dia_2/exercises_parte_2/script.js
+++ dia_2/exercises_parte_2/script.js
@@ -19,30 +19,40 @@ const lesson1 = {
   };
 
   // Exercício 1
-  const turno = (objeto, chave, valor) => {
-     let newKey = chave;
-     chave = valor;
-     objeto[newKey] = chave;
+//   const turno = (objeto, chave, valor) => {
+//      let newKey = chave;
+//      chave = valor;
+//      objeto[newKey] = chave;
 
-    return Object.values(objeto);
-  }
-  console.table(turno(lesson2, 'turno', 'manhã'));
+//     return Object.values(objeto);
+//   }
+//   console.table(turno(lesson2, 'turno', 'manhã'));
 
-  // EXercício 2
-  const listandoChaves = (objeto) => Object.keys(objeto);
-  console.log(listandoChaves(lesson3));
+//   // EXercício 2
+//   const listandoChaves = (objeto) => Object.keys(objeto);
+//   console.log(listandoChaves(lesson3));
 
-  // Exercício 3
-  const comprimentoDoObjeto = (objeto) => Object.keys(objeto).length;
-  console.log(comprimentoDoObjeto(lesson1));
+//   // Exercício 3
+//   const comprimentoDoObjeto = (objeto) => Object.keys(objeto).length;
+//   console.log(comprimentoDoObjeto(lesson1));
 
-  // Exercício 4
-  const listandoValores = (objeto) => Object.values(objeto);
-  console.log(listandoValores(lesson2));
+//   // Exercício 4
+//   const listandoValores = (objeto) => Object.values(objeto);
+//   console.log(listandoValores(lesson2));
 
   // Exercício 5
   const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
   console.log(allLessons);
+
   // Exercício 6
+  const totalDeEstudantes = (objeto) => {
+    const estudantes_1 = objeto.lesson1.numeroEstudantes;
+    const estudantes_2 = objeto.lesson2.numeroEstudantes;
+    const estudantes_3 = objeto.lesson3.numeroEstudantes;
+    const total = estudantes_1 + estudantes_2 + estudantes_3;
+
+    return `Total de Estudantes: ${total}`;
+  }
+ console.log(totalDeEstudantes(allLessons));
   // Exercício 7
   // Exercício 8
