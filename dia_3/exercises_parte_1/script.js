2c50d37304566a66da560f7df3ce7b709ebafb0b
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-26 16:49:44 -0300

    Requisito 1

--- dia_3/exercises_parte_1/script.js
+++ dia_3/exercises_parte_1/script.js
@@ -37,3 +37,26 @@ assert.deepStrictEqual(myRemove([1, 2, 3, 4, 5], 5), [1, 2, 3, 4]);
 // assert.deepStrictEqual(array, [1, 2, 3, 4]);
 // assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 // assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+
+// Exercício 3 
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// implemente seus testes aqui
+// assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+// assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// const array3 = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(myRemoveWithoutCopy(array3, 5));
+
