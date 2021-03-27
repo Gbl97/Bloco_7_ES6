00ccba1333040941be9ed7668c3f9dbb91e1ae60
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-27 12:59:43 -0300

    Exercicio 4 da parte II - Feito

--- dia_3/exercises_parte_2/script.js
+++ dia_3/exercises_parte_2/script.js
@@ -57,3 +57,32 @@ const output = sumAllNumbers(numbers);
 
 assert.strictEqual(typeof sumAllNumbers, 'function');
 assert.strictEqual(output, expected);
+
+// Exercício 4 
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+const findTheNeedle = (array, word) => {
+    let getIndex = -1;
+
+    for (let index = 0; index < array.length; index += 1) {
+        if (array[index] === word) {
+            getIndex = index;
+        }
+    }
+    return getIndex;
+}
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
