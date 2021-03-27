34ce18ec29661c9c3cab89a623e8136094c09035
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-27 12:48:39 -0300

    Exercicio 3 da parte II - Feito

--- dia_3/exercises_parte_2/script.js
+++ dia_3/exercises_parte_2/script.js
@@ -38,3 +38,22 @@ const expected = [3, 6, 10, 5];
 assert.strictEqual(typeof wordLengths, 'function');
 const output = wordLengths(words);
 assert.deepStrictEqual(output, expected);
+
+// Exercício 3
+const assert = require('assert');
+// escreva a função sumAllNumbers aqui
+const sumAllNumbers = (array) => {
+    let soma = 0;
+
+    for (let index = 0; index < array.length; index += 1) {
+        soma += array[index];
+    }
+    return soma;
+}
+
+const numbers = [9, 23, 10, 3, 8];
+const expected = 53;
+const output = sumAllNumbers(numbers);
+
+assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output, expected);
