256ef70206741d7cd2a4130f5a639781e712ceb7
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-03 15:19:22 -0300

    Exercicio 3 e 4 da parte III

--- dia_3/exercises_parte_3/script.js
+++ dia_3/exercises_parte_3/script.js
@@ -31,9 +31,37 @@ const removeVowels = (word) => {
     }
     return results.join('');
   };
-  const parameter = 'Dayane';
-  const result = 'D1y2n3';
-  assert.deepStrictEqual(removeVowels(parameter), result);
+//   const parameter = 'Dayane';
+//   const result = 'D1y2n3';
+//   assert.deepStrictEqual(removeVowels(parameter), result);
 //   console.log(removeVowels(parameter));
+
 // Exercício 3
+const greaterThanTen = (array) => {
+    let results = [];
+    for (let index = 0; index < array.length; index += 1) {
+      if (array[index] > 10) {
+        results.push(array[index]);
+      }
+    }
+    return results;
+  };
+//   const parameter = [4, 10, 32, 9, 21];
+//   const result = [32, 21];
+//   assert.deepStrictEqual(greaterThanTen(parameter), result);
+//   console.log(greaterThanTen(parameter));
+
 // Exercício 4
+function secondThirdSmallest(array) {
+    let results = []
+    array.sort(function (x, y) {
+        return x - y;
+    });
+    results = [array[1], array[2]];
+    return results;
+};
+
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
+assert.deepStrictEqual(secondThirdSmallest(parameter), result);
+// console.log(secondThirdSmallest(parameter));
